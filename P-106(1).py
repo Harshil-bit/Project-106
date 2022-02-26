0ab02d7b52536347711fb750a61c18e2750a0a01
Harshil-bit <75490038+Harshil-bit@users.noreply.github.com>
2022-02-26 16:48:24 +0530

    Update P-106(1).py

--- P-106(1).py
+++ P-106(1).py
@@ -1,6 +1,6 @@
 import plotly.express as px
 import csv
-with open('py/cupsofcoffeevshoursofsleep.csv') as f:
+with open('cupsofcoffeevshoursofsleep.csv') as f:
     df=csv.DictReader(f)
     fig=px.scatter(df,x="Coffee in ml",y="sleep in hours")
     fig.show()
