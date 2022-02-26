75ffde61a237fdb878aad4e2d38bd25acd4a1d2f
Harshil-bit <75490038+Harshil-bit@users.noreply.github.com>
2022-02-26 16:48:45 +0530

    Update P-106(2).py

--- P-106(2).py
+++ P-106(2).py
@@ -1,6 +1,6 @@
 import plotly.express as px
 import csv
-with open('py/Student MarksvsDays Present.csv') as f:
+with open('Student MarksvsDays Present.csv') as f:
     df=csv.DictReader(f)
     fig=px.scatter(df,x="Marks In Percentage",y="Days Present")
     fig.show()
